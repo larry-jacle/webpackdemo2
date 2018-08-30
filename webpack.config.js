a74c88bb0a31308bb783e4a5afa901248d23ffbd
larry-jacle <jijj19@163.com>
2018-08-30 16:14:38 +0800

    add babel relative

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin=require("html-webpack-plugin");
 var path=require("path");
 
+
+/**
+ * babel-loader: 负责es6语法转化
+ * babel-preset-env: 包含es6、7等版本的语法转化规则
+ * babel-polyfill: es6内置方法和函数转化垫片
+ * babel-plugin-transform-runtime: 避免polyfill污染全局变量
+ * 更多资料：
+ * polyfill引入：https://www.babeljs.cn/docs/usage/polyfill/
+ * babel-preset-env配置：https://www.babeljs.cn/docs/plugins/preset-env/
+ */
 module.exports={
     entry:{app:"./src/js/index.js"},
     output:{
@@ -26,6 +36,15 @@ module.exports={
                     //loader: 'file-loader'
                 }
             ]
+        },{
+            test :/(\.jsx|\.js)$/,
+            exclude : /node_modules/,
+            loader :'babel-loader',
+            options:{
+                presets:[
+                    "env"
+                ]
+            }
         }],
 
     },
